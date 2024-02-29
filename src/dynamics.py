38ab73430f0c1425bf94df69f34c4cc017bdfdda
comecattin <cattin.come@gmail.com>
2024-02-29 18:20:25 +0100

    JAX modification of the forces.

--- src/dynamics.py
+++ src/dynamics.py
@@ -3,17 +3,27 @@
 import jax
 import jax.numpy as jnp
 
-
-def step(xyz, velocities, forces, dt, masses):
+@jax.jit
+def step(
+        xyz,
+        velocities,
+        dt,
+        masses,
+        cutoff,
+        epsilon
+    ):
     """Perform a single step of molecular dynamics."""
     dt2 = 0.5 * dt
     dt2m = jnp.asarray(dt2 / masses[:, None], dtype=jnp.float32)
+    forces = compute_forces(xyz, cutoff, epsilon)
     velocities = velocities + forces * dt2m
     xyz = xyz + velocities * dt
+    forces = compute_forces(xyz, cutoff, epsilon)
+    velocities = velocities + forces * dt2m
     return xyz, velocities
 
 
-def compute_forces(xyz, cutoff,epsilon):
+def compute_forces(xyz, cutoff, epsilon):
     """Compute the forces using the Lennard-Jones potential."""
     forces = jnp.zeros_like(xyz)
 
@@ -28,10 +38,11 @@ def compute_forces(xyz, cutoff,epsilon):
                         ) / dist
                     )
 
-                forces[i] -= force
-                forces[j] += force
+                forces = forces.at[i].add(-force)
+                forces = forces.at[j].add(force)
 
     return forces
+
 
 if __name__ == "__main__":
     xyz_input = jnp.array(
@@ -53,3 +64,6 @@ if __name__ == "__main__":
     )
 
     atomic_masses = jnp.array([16, 1, 1])
+    epsilon = 1e-5
+    cutoff = 3.0
+    dt = 0.1
