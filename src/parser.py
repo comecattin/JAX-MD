68426ce75693bd6b7ec0cd59062a29c0ef314f30
comecattin <cattin.come@gmail.com>
2024-03-07 09:26:27 +0100

    Docstrings.

--- src/parser.py
+++ src/parser.py
@@ -9,29 +9,59 @@ import jax.numpy as jnp
 import yaml
 
 
-def get_input_file():
-    parser = argparse.ArgumentParser(
-        description='Parser for input file for the dynamic.'
-    )
-    parser.add_argument(
-        'param_file',
-        type=Path,
-        help='This is the input file for the dynamic'
-    )
-    return parser.parse_args().param_file
+class Parser:
+    """Parser for the input file for the dynamic."""
 
+    def __init__(self):
+        self.file = self.get_input_file()
+        self.arguments = self.get_argument()
 
-def get_argument(file):
-    """Get the argument from the yaml file."""
-    with open(file) as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+    def get_input_file(self):
+        """Get the input file for the dynamic.
+
+        The input file is a yaml file.
+        """
+        parser = argparse.ArgumentParser(
+            description='Parser for input file for the dynamic.'
+        )
+        parser.add_argument(
+            'param_file',
+            type=Path,
+            help='This is the input file for the dynamic'
+        )
+        return parser.parse_args().param_file
+
+    def get_argument(self):
+        """Get the argument from the input file.
+
+        Returns
+        -------
+            dict: Dictionary of the arguments.
+        """
+        with open(self.file) as stream:
+            try:
+                return yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
 
 
 def read_xyz_atomic(file):
-    """Read the xyz file."""
+    """Extract system information from a xyz file.
+
+    Parameters
+    ----------
+    file : str
+        Path to the xyz file.
+
+    Returns
+    -------
+    num_particule : int
+        Number of particules in the system.
+    comment : str
+        Comment of the system.
+    pos : jnp.array
+        Position of the particules in the system.
+    """
     with open(file) as f:
         lines = f.readlines()
         num_particule = int(lines[0])
@@ -46,9 +76,11 @@ def read_xyz_atomic(file):
 
 
 if __name__ == "__main__":
-    file = 'system.xyz'
+    file = 'data/xyz/system.xyz'
     num_particule, comment, pos = read_xyz_atomic(file)
     # file = get_input_file()
     # argument = get_argument(file.param_file)
     # key = argument['key']
     # print(key)
+    parser = Parser()
+    print(parser.arguments)
